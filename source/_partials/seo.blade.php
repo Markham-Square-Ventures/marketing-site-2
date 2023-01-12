3a851d865ad018584cba1e9d5d4ccc837fcdb810
Nick Basile <nicholas.basile@outlook.com>
2023-01-12 11:03:14 -0600

    wip

--- source/_partials/seo.blade.php
+++ source/_partials/seo.blade.php
@@ -2,19 +2,19 @@
 <meta name="description" content="{{ $description }}">
 
 <!-- /* Open Graph tags for FB & Twitter */ --->
-<meta name="og:title" content="{{ $title }}">
-<meta name="og:description" content="{{ $description }}">
-<meta name="og:type" content="website">
-<meta name="og:url" content="{{ $page->getUrl() }}">
-<meta name="og:image" content="{{ $image }}">
-<meta name="og:image:alt" content="{{ $image_alt }}">
-<meta name="og:site_name" content="Markham Square">
+<meta property="og:title" content="{{ $title }}">
+<meta property="og:description" content="{{ $description }}">
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ $page->getUrl() }}">
+<meta property="og:image" content="{{ $image }}">
+<meta property="og:image:alt" content="{{ $image_alt }}">
+<meta property="og:site_name" content="Markham Square">
 
 <!-- /* Twitter Large Image Summary Card */ -->
-<meta name="twitter:title" content="{{ $title }}">
-<meta name="twitter:description" content="{{ $description }}">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@team_markhamsq">
 <meta name="twitter:creator" content="@team_markhamsq">
+<meta name="twitter:title" content="{{ $title }}">
+<meta name="twitter:description" content="{{ $description }}">
 <meta name="twitter:image" content="{{ $image }}">
 <meta name="twitter:image:alt" content="{{ $image_alt }}">
