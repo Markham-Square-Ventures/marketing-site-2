a64e95c8b3f7d040b774af6b60a27ed841d5118b
Nick Basile <nicholas.basile@outlook.com>
2023-05-25 16:38:13 -0500

    wip

---
+++ source/_partials/image-card-large.blade.php
@@ -0,0 +1,7 @@
+<div class="col-span-2">
+    <img src="{{ $src }}" alt="{{ $alt }}" class="w-full h-auto">
+    <div class="mt-5">
+        <h2 class="text-7 text-white -tracking-[1px] leading-[100%]">{{ $title }}</h2>
+        <p class="mt-2 text-5 text-neutral-200 -tracking-[1px] leading-[100%] font-light">{{ $subtitle }}</p>
+    </div>
+</div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -5,8 +5,31 @@
         @include('_partials.nav')
         @include('_partials.hero', [
             'title' => 'We help teams succeed on the web',
-            'subtitle' => 'Markham Square is a digital agency specialized in designing & building digital experiences for startups &
-                    established organizations.',
+            'subtitle' =>
+                'Markham Square is a digital agency specialized in designing & building digital experiences for startups & established organizations.',
         ])
+
+        <section class="bg-tile bg-repeat px-7 lg:px-0 pt-10 pb-20 flex justify-center ">
+            <div class="w-full max-w-[1000px] grid grid-cols-2 gap-10">
+                @include('_partials.image-card-large', [
+                    'src' => '/assets/images/fanxp.jpg',
+                    'alt' => 'A screenshot of the FanXP dashboard, it\'s quite good.',
+                    'title' => 'FanXP',
+                    'subtitle' => 'Building a platform where fandom is rewarded',
+                ])
+                @include('_partials.image-card-small', [
+                    'src' => '/assets/images/lynden-lane.jpg',
+                    'alt' => 'A screenshot of the Lynden Lane website, it\'s trés chic.',
+                    'title' => 'Lynden Lane',
+                    'subtitle' => 'A new home for transformational events & interiors',
+                ])
+                @include('_partials.image-card-small', [
+                    'src' => '/assets/images/soldev.jpg',
+                    'alt' => 'A screenshot of the SolDev website, mamma mia.',
+                    'title' => 'SolDev',
+                    'subtitle' => 'Bringing structure & style to the best Solana content',
+                ])
+            </div>
+            <section>
     </header>
 @endsection
